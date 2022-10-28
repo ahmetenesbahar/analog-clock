628328b1278f181a0d35002b091619d9b0dde1e8
ahmetenesbahar <enesbahar2303@gmail.com>
2022-10-28 19:13:05 +0300

    styling done

---
+++ script.js
@@ -0,0 +1,6 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const hourEl = document.querySelector(".hour");
+const timeEl = document.querySelector(".time");
+const hourEl = document.querySelector(".hour");
